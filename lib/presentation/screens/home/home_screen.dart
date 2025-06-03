c863e7471d0b6a71ad3a0aa77fe0cbaafee9fc0c
Russ DEV <niahui.dev25@gmail.com>
2025-06-03 13:50:52 -0500

    navegacion de rutas con nombre implementadas con go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+//import 'package:widgets_app/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -47,7 +50,9 @@ class _CustomListTitle extends StatelessWidget {
         // Navigator.of(
         //   context,
         // ).push(MaterialPageRoute(builder: (context) => ButtonsScreen()));
-        Navigator.pushNamed(context, menuItem.link);
+        //Navigator.pushNamed(context, menuItem.link);
+        //context.push(menuItem.link);
+        context.push(menuItem.link); // Use the name of the route
       },
     );
   }
