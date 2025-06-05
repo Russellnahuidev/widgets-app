855419bb6d5301b5b5fabab40f9ee8883736cebb
Russell ÑAHUI <russellnahui.dev25@gmail.com>
2025-06-04 20:46:16 -0500

    infiniteScroll terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -58,4 +58,10 @@ const appMenuItems = <MenuItems>[
     link: '/tutorial',
     icon: Icons.accessible_rounded,
   ),
+  MenuItems(
+    title: 'InfiniteScroll y Pull',
+    subTitle: 'Listas infinitas y pull to refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_rounded,
+  ),
 ];
